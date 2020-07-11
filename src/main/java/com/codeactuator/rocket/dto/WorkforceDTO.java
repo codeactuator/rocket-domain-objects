f8948b4776bc35f8d26824ef2392a038ab3e404d
Shekhar Kumar <shekharkumargupta@gmail.com>
2020-07-12 05:06:44 +0530

    added ProjectDTO and WorkforceDTO

---
+++ src/main/java/com/codeactuator/rocket/dto/WorkforceDTO.java
@@ -0,0 +1,18 @@
+package com.codeactuator.rocket.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class WorkforceDTO {
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String designation;
+}
