c238c94d511555e7e87a7c8cb47b169bc29e95e0
Shekhar Kumar <shekharkumargupta@gmail.com>
2020-07-25 01:49:17 +0530

    Created a global ErrorCode class

---
+++ src/main/java/com/codeactuator/rocket/constants/ErrorCodes.java
@@ -0,0 +1,39 @@
+package com.codeactuator.rocket.constants;
+
+public class ErrorCodes {
+
+
+
+    public enum PROJECT {
+
+        PROJECT_NOT_FOUND("Could not find the project!");
+
+        private String message;
+        private PROJECT(String message){
+            this.message = message;
+        }
+
+        @Override
+        public String toString() {
+            return  message;
+        }
+    };
+
+
+    public enum WORKFORCE {
+
+        WORKFORCE_NOT_FOUND("Could not find the workforce!");
+
+        private String message;
+        private PROJECT(String message){
+            this.message = message;
+        }
+
+        @Override
+        public String toString() {
+            return  message;
+        }
+    };
+
+
+}
