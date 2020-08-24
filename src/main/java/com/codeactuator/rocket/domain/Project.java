2a4d2bb3ec14aabc7c98ef4c1f432b05bc44fb85
Shekhar Kumar <shekharkumargupta@gmail.com>
2020-08-25 02:10:02 +0530

    Relation mapping with Task is corrected to be bi-directional

--- src/main/java/com/codeactuator/rocket/domain/Project.java
+++ src/main/java/com/codeactuator/rocket/domain/Project.java
@@ -21,7 +21,7 @@ public class Project {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    @OneToMany(mappedBy = "true")
+    @OneToMany(mappedBy = "project")
     private Set<Task> tasks;
     @ManyToMany
     private Set<Workforce> workforces;
